4fc9d2087ca5687a8f6c0d4589dd38e8448cd6d2
Mille Wanyanga <wanyangamille@gmail.com>
2025-05-21 16:23:46 +0300

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define the variables
+number1 = 10
+number2 = 5
+
+# Calculating the sum, difference, and product
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Printing the results
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
